6518741ddab9c8f1668a4bf1fb736846611a2317
MatheusVict <matheusvictorhenrique@gmail.com>
2023-12-15 20:12:40 -0300

    chore: add swagger contact
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/br/com/threewings/wingsblog/config/Beans.java
@@ -0,0 +1,24 @@
+package br.com.threewings.wingsblog.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Beans {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("3Wings Blog API Challenge")
+                        .description("API for a blog challenge with react and spring boot")
+                        .version("1.0").contact(new Contact().name("Matheus Victor")
+                                .email("matheusvictorhenrique@gmail.com")
+                                .url("https://github.com/MatheusVict/3wings-full-stack-challenge"))
+                        .license(new License().name("License of API")
+                                .url("https://opensource.org/license/mit/")));
+    }
+}
