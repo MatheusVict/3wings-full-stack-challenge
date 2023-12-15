ef4f72d66fcb6301c45ebfb1fb13c03165f39d06
MatheusVict <matheusvictorhenrique@gmail.com>
2023-12-15 13:40:01 -0300

    feat: post repository
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/br/com/threewings/wingsblog/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package br.com.threewings.wingsblog.repository;
+
+import br.com.threewings.wingsblog.domain.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
