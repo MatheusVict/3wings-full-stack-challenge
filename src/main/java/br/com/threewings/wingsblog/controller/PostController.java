d02df0977bfa60ef1fee7aea36c1843ed72dd9b7
MatheusVict <matheusvictorhenrique@gmail.com>
2023-12-15 20:35:17 -0300

    chore: adding response swagger on endpoints
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/br/com/threewings/wingsblog/controller/PostController.java
+++ src/main/java/br/com/threewings/wingsblog/controller/PostController.java
@@ -5,8 +5,12 @@ import br.com.threewings.wingsblog.dto.CreatePostDTO;
 import br.com.threewings.wingsblog.dto.UpdatePostDTO;
 import br.com.threewings.wingsblog.service.PostService;
 import br.com.threewings.wingsblog.service.impl.PostServiceImpl;
+import br.com.threewings.wingsblog.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,29 +23,46 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping
-    public List<Post> findAll() {
-        return this.postService.findAll();
+    @Tag(name = "Post")
+    @OkResponse
+    public ResponseEntity<List<Post>> findAll() {
+        return ResponseEntity.ok(this.postService.findAll());
     }
 
     @PostMapping
-    public void save(@RequestBody @Valid CreatePostDTO postDTO) {
+    @Tag(name = "Post")
+    @CreatedResponse
+    @ConflictResponse
+    public ResponseEntity<?> save(@RequestBody @Valid CreatePostDTO postDTO) {
         Post post = postDTO.toEntity();
         this.postService.save(post);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/{id}")
-    public Post findById(@PathVariable Long id) {
-        return this.postService.findById(id);
+    @Tag(name = "Post")
+    @OkResponse
+    @NotFoundResponse
+    public ResponseEntity<Post> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.postService.findById(id));
     }
 
     @PutMapping("/{id}")
-    public void update(@RequestBody UpdatePostDTO postDTO, @PathVariable Long id) {
+    @Tag(name = "Post")
+    @NoContentResponse
+    @NotFoundResponse
+    public ResponseEntity<?> update(@RequestBody UpdatePostDTO postDTO, @PathVariable Long id) {
         Post post = postDTO.toEntity();
         this.postService.update(post, id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    @Tag(name = "Post")
+    @NoContentResponse
+    @NotFoundResponse
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         this.postService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/br/com/threewings/wingsblog/dto/UpdatePostDTO.java
+++ src/main/java/br/com/threewings/wingsblog/dto/UpdatePostDTO.java
@@ -2,9 +2,12 @@ package br.com.threewings.wingsblog.dto;
 
 import br.com.threewings.wingsblog.domain.post.Post;
 import br.com.threewings.wingsblog.utils.SlugConversions;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UpdatePostDTO(
+        @Schema(description = "Post title", example = "My first post")
         String title,
+        @Schema(description = "Post content", example = "This is my first post")
         String content
 ) {
     public Post toEntity() {
