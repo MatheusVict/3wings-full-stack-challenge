2f4aaab0734ed206ae033dc1b74d4ee502c873c6
MatheusVict <matheusvictorhenrique@gmail.com>
2023-12-19 14:28:18 -0300

    fix: cors methods alloweds
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/br/com/threewings/wingsblog/config/Beans.java
+++ src/main/java/br/com/threewings/wingsblog/config/Beans.java
@@ -29,7 +29,10 @@ public class Beans {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
